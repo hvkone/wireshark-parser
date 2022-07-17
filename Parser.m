460361b8eb914cb0438f6d11cd1a7de3782a1bb4
hvkone <wiljevaiv@outlook.com>
2022-07-17 08:19:49 -0500

    edit output of data via wireshark

--- Parser.m
+++ Parser.m
@@ -5,5 +5,20 @@ clc; close all; format long;
 [file,path,indx] = uigetfile({'*.csv'},'Select Wireshark Data');
 dir_data = strcat(path,file);
 %% Read .csv File
-[csv_data, delimiterOut, headerlinesOut] = importdata(dir_data);
+csv_data = readcell(dir_data);
+cell2mat(csv_data);
+%% List Platforms' IP Addresses
+P8 
+E2
+F18
+MQ25
+%% Sort Data
+for all_rows = 2:size(csv_data,1)
+    if (csv_data{all_rows,3}  == '13.107.42.12' && csv_data{all_rows,4}  == '13.107.42.12')
+        p1_source(all_rows) = csv_data(all_rows,3);
+        p1_destin(all_rows) = csv_data(all_rows,4);
+    end
+%     p2_source(all_rows) =
+%     p2_destin(all_rows) =
+end
 
